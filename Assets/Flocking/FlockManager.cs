68305763646e04b08610840001917d0814453083
Sp1derMonkey04 <115369325+Sp1derMonkey04@users.noreply.github.com>
2025-02-03 23:34:58 +0000

    Testing cohesion functionality
    Looking at cohesion not being EVERY entities position averaged, but only the entities close to each other. Will have to rewrite what I've currently got to do that I think

--- Assets/Flocking/FlockManager.cs
+++ Assets/Flocking/FlockManager.cs
@@ -10,9 +10,12 @@ public class FlockingManager : MonoBehaviour
     public FlockingEntity[] flock;
     public FlockingEntity flockMember;
 
+    //The radius at which flocking entities can detect eachother
+    public float detectionRadius;
 
     //Value of average position of all flock members
     protected Vector3 averagePosition = Vector3.zero;
+    protected Vector3 centrePosition = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -32,24 +35,27 @@ public class FlockingManager : MonoBehaviour
         Vector3 averagePosition = new Vector3 (0,0,0);
         foreach (var entity in flock)
         {
-            averagePosition += entity.transform.position;
+
+            //This could probably work, but would need to move functionality into each entitiy instead of it being in the manager
+            foreach (var otherEntitiy in flock)
+            {
+                if ((entity != otherEntitiy) && ((entity.transform.position - otherEntitiy.transform.position).magnitude < detectionRadius))
+                {
+                    averagePosition += entity.transform.position;
+                }
+            }
+
+
+            //averagePosition += entity.transform.position;
         }
-        Vector3 centrePosition = averagePosition / flock.Length;
-        //Debug.Log(averagePosition);
+        centrePosition = averagePosition / flock.Length;
+        Debug.Log(centrePosition);
 
+        Coherence();
 
+        Seperation();
 
-        //Cohesion calculation - Takes the Average position of all cubes, calculates each cubes unit vector towards it,
-        //then adds a force to make them head there
-        foreach (var entity in flock)
-        {
-            //Unit vector from current cube in loop to the centre
-            Vector3 unitVecTowardsCentre = (1 / (centrePosition - entity.transform.position).magnitude) * (centrePosition - entity.transform.position);
-
-            //Adding an impulse to head towards the centre
-            entity.rBody.AddForce(unitVecTowardsCentre);
-            Debug.Log(unitVecTowardsCentre);
-        }
+        Alignment();
 
 
 
@@ -58,4 +64,51 @@ public class FlockingManager : MonoBehaviour
 
 
     }
+
+    void Coherence()
+    {
+        //Cohesion calculation - Takes the Average position of all cubes, calculates each cubes unit vector towards it,
+        //then adds a force to make them head there
+        foreach (var entity in flock)
+        {
+
+
+
+
+            //Unit vector from current cube in loop to the centre
+            Vector3 unitVecTowardsCentre = (1 / (centrePosition - entity.transform.position).magnitude) * (centrePosition - entity.transform.position);
+
+            //Adding an impulse to head towards the centre
+            entity.rBody.AddForce(unitVecTowardsCentre);
+            //Debug.Log(unitVecTowardsCentre);
+        }
+
+
+        //for (int i = 0; i < flock.length; i++) 
+        //{
+        //    foreach
+
+
+
+        //}
+
+
+
+    }
+
+    void Seperation()
+    {
+
+
+
+    }
+
+    void Alignment ()
+    {
+
+
+
+    }
+
+
 }
