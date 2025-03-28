4d693ae0d3306c8ae4e7101af16df5bc5f9adf05
mikeyread <123581961+mikeyread@users.noreply.github.com>
2025-03-28 19:04:34 +0000

    render problem

---
+++ Assets/Pathfinding/thing/NewBehaviourScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public UnityEngine.Vector3[] vertices;
+    int[] indices;
+    Mesh mesh;
+
+
+    private void Awake()
+    {
+        // Initialize in the Awake function
+        vertices = new UnityEngine.Vector3[4];
+        indices = new int[6];
+        mesh = new Mesh();
+
+        this.AddComponent<MeshFilter>();
+        this.AddComponent<MeshRenderer>();
+        this.GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+
+    }
+    void CreateMesh ()
+    {
+        
+        vertices[0] = new Vector3(-1, 0, -1) + this.transform.position;
+        vertices[1] = new Vector3(1, 0, -1) + this.transform.position;
+        vertices[2] = new Vector3(-1, 0, 1) + this.transform.position;
+        vertices[3] = new Vector3(1, 0, 1) + this.transform.position;
+
+        indices[0] = 1;
+        indices[1] = 0;
+        indices[2] = 2;
+        indices[3] = 2;
+        indices[4] = 3;
+        indices[5] = 1;
+
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+
+        this.GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
